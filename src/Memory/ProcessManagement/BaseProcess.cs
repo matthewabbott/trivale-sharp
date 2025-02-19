d64075c9e9d84eac3736009cb97c84ddcd57de4d
matthewabbott <ttobbatam@gmail.com>
2025-02-19 09:52:31 +0000

    Separate out BaseProcess

---
+++ src/Memory/ProcessManagement/BaseProcess.cs
@@ -0,0 +1,50 @@
+// src/Memory/ProcessManagement/BaseProcess.cs
+namespace Trivale.Memory.ProcessManagement;
+
+/// <summary>
+/// Base implementation of IProcess that handles common functionality.
+/// </summary>
+public abstract class BaseProcess : IProcess
+{
+    public string Id { get; }
+    public abstract string Type { get; }
+    public virtual Dictionary<string, float> ResourceRequirements => new();
+    public bool IsComplete { get; protected set; }
+    
+    protected Dictionary<string, object> State { get; private set; }
+    
+    public event Action<Dictionary<string, object>> StateChanged;
+    
+    protected BaseProcess(string id)
+    {
+        Id = id;
+        State = new Dictionary<string, object>();
+    }
+    
+    public virtual void Initialize(Dictionary<string, object> initialState)
+    {
+        State = initialState ?? new Dictionary<string, object>();
+        OnInitialize();
+    }
+    
+    public virtual void Update(float delta)
+    {
+        OnUpdate(delta);
+    }
+    
+    public virtual void Cleanup()
+    {
+        OnCleanup();
+    }
+    
+    public Dictionary<string, object> GetState() => new(State);
+    
+    protected virtual void OnInitialize() { }
+    protected virtual void OnUpdate(float delta) { }
+    protected virtual void OnCleanup() { }
+    
+    protected void EmitStateChanged()
+    {
+        StateChanged?.Invoke(GetState());
+    }
+}
